e6ecba37775876a2519516bf3bf5181e8ebd6679
unknown <jeffreyhshapiro@gmail.com>
2016-03-13 22:27:10 -0400

    one article at a time is shown in the webpage

--- controller/dataInfoHandler.js
+++ controller/dataInfoHandler.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
 
+//All data is sent straight to the browser. Every time the read article button is clicked a new article is shown.  Counter starts at -1 so the article at index 0 can be shown.
+
+  var counter = -1;
+
   $("#getArticle").on("click", function(){
+
+    counter++
+    $("#results").empty();
+
     $.getJSON('/articles', function(data){
       for (var i = 0; i < data.length; i++) {
 
-        // var row = $('<div>').addClass('row')
-        // var col = $('<div>').addClass('col s12 m6')
-        // var cardColor = $('<div>').addClass('card blue-grey darken-1')
-        // var textColor = $('<div>').addClass('card-content white-text')
-        // var cardTitle = $('<span>').addClass('card-title')
-        // var cardBody = $('<p>').append('text text text')
+        console.log('the value of counter is '+counter)
 
-        // $("#results").append(row).append(col).append(cardColor).append(textColor).append(cardTitle).append(cardBody)
-        $("#getArticle").hide();
-        var headline = $('<h5>').addClass('col s6 m6').append(data[i].article);
-        var link = $('<a>').attr('href',data[i].link).attr('target', '_blank');
+        var headline = $('<h5>').addClass('col s6 m6').append(data[counter].article);
+        var link = $('<a>').attr('href',data[counter].link).attr('target', '_blank');
         var headlineComplete = $(link).append(headline);
-        var description = $('<p>').append(data[i].description);
+        var description = $('<p>').append(data[counter].description);
         var formTag = $('<form>').attr('method', 'POST').attr('action','/submitcomment');
         var divForForm = $('<div>').addClass('input-field col-s6');
         var inputForForm = $('<input>').attr('placeholder','Comment here!').attr('type', 'text').attr('name','scienceComment');
@@ -24,12 +25,17 @@ $(document).ready(function(){
         var submitButton = $('<button>').attr('type','submit').addClass('waves-effect waves-light btn').attr('id','test').append('Submit');
         var commentField = $(formTag).append(divForForm).append(inputForForm).append(labelForForm).append(submitButton);
 
-        $('#results').append(headlineComplete).append(data[i].description).append(commentField)
+        $('#results').append(headlineComplete).append(description).append(commentField)
 
-        console.log(data[i].article);
-        console.log(data[i].description);
-        console.log(data[i].link);
+        // console.log(data[i].article);
+        // console.log(data[i].description);
+        // console.log(data[i].link);
 
+        if (counter == data.length - 1) {
+          counter = -1
+        };
+
+        break;
       };
     });
   });
